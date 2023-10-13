e49ea12b5e72dd1e4a34f1102a60aade44cd7a34
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-10-13 22:15:44 +0000

    Updated num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -2,14 +2,14 @@
 
 # Created By: Julien Lamoureux
 # Date: October 13, 2023
-# This program is a number guessing for whole number between 1 and 10
+# This program is a number guessing for whole number between 0 and 9
 import constants
 
 
 def main():
     # get the user guess
     print(
-        "In this game, a correct integer has been chosen from 1 to 10. Try to guess the correct integer."
+        "In this game, a correct integer has been chosen from 0 to 9. Try to guess the correct integer."
     )
     user_guess = int(input("What is your guess for the correct integer: "))
 
